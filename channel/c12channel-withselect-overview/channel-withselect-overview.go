78f18282cebd7f9a19c993ad0c9ccd3a7a2c71e6
l898974766@outlook.com <l898974766@outlook.com>
2023-11-21 19:34:46 +0800

    20231121

--- channel/c12channel-withselect-overview/channel-withselect-overview.go
+++ channel/c12channel-withselect-overview/channel-withselect-overview.go
@@ -1,23 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func Generate() {
+func Generate() <-chan int {
+	out := make(chan int)
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			out <- i
+			i++
+		}
+	}()
+	return out
+}
 
+type worker struct {
+	in   chan int
+	done chan bool
+}
+
+func doWorker(id int, c chan int, done chan bool) {
+	for n := range c {
+		fmt.Printf("worker %d received data %d \n", id, n)
+		// 通知外部，本函数完成任务
+		done <- true
+	}
+}
+
+func createWorker(i int) worker {
+	w := worker{
+		in:   make(chan int),
+		done: make(chan bool),
+	}
+	go doWorker(i, w.in, w.done)
+	return w
 }
 
 func SelectOverview() {
 	// select 的使用场景，我们有多个 channel
-	var c1, c2 chan int
+	var c1, c2 = Generate(), Generate()
+	// 我们一个 worker
+	w := createWorker(1)
 	// 当我们想从多个 channel 中收取数据，谁快用谁，这时，可以使用select
-	for {
+	for i := 0; i < 50; i++ {
 		select {
 		case n := <-c1:
-			fmt.Printf("\n received value from c1 %d \n", n)
+			// fmt.Printf("\n received value from c1 %d \n", n)
+			// 但是这里 w.in <- n 是阻塞的？
+			/*
+				The statement n <- c is trying to receive a value from channel c.
+				If there's no value available on channel c,
+				this operation will wait until a value is sent on that channel.
+				Until then, the execution of the program will be blocked at this point.
+			*/
+			w.in <- n
+			<-w.done
 		case n2 := <-c2:
-			fmt.Printf("\n received value from c2 %d \n", n2)
+			// fmt.Printf("\n received value from c2 %d \n", n2)
+			w.in <- n2
+			<-w.done
 		default:
 			fmt.Print(" ... ")
+			time.Sleep(time.Duration(7*100) * time.Millisecond)
 		}
 	}
 
---
+++ channel/c14channel-withselect-overview-refactor2/channel-withselect-overview-refactor2.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Generate() <-chan int {
+	out := make(chan int)
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			out <- i
+			i++
+		}
+	}()
+	return out
+}
+
+type worker struct {
+	in   chan int
+	done chan bool
+}
+
+func doWorker(id int, c chan int, done chan bool) {
+	for n := range c {
+		fmt.Printf("worker %d received data %d \n", id, n)
+	}
+}
+
+func createWorker(i int) worker {
+	w := worker{
+		in:   make(chan int),
+		done: make(chan bool),
+	}
+	go doWorker(i, w.in, w.done)
+	return w
+}
+
+func SelectOverviewRefactor2() {
+	// select 的使用场景，我们有多个 channel
+	var c1, c2 = Generate(), Generate()
+
+	var worker = createWorker(0)
+	n := 0
+	hasValue := false
+	for i := 0; i < 50; i++ {
+		// nil channel 能正确运行，但是不会被 select 到
+		var activeWorker chan int
+		if hasValue {
+			activeWorker = worker.in
+		}
+		select {
+		case n = <-c1:
+			hasValue = true
+		case n = <-c2:
+			hasValue = true
+			// 但是，如果 c1, c2 生产数据的速度大于 activeWorker 消费数据的速度
+			// n 将会无法接收，所以此程序依旧有问题
+		case activeWorker <- n:
+			hasValue = false
+		}
+	}
+
+}
+
+func main() {
+	SelectOverviewRefactor2()
+}
