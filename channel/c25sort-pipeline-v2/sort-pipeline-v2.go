4b7ab9147ed4cca3cd38dfa52195402b1b95d263
l898974766@outlook.com <l898974766@outlook.com>
2023-12-06 18:56:51 +0800

    20231206

--- channel/c25sort-pipeline-v2/sort-pipeline-v2.go
+++ channel/c25sort-pipeline-v2/sort-pipeline-v2.go
@@ -46,7 +46,18 @@ func Merge(in1, in2 <-chan int) <-chan int {
 	out := make(chan int)
 
 	go func() {
-		
+		v1, ok1 := <-in1
+		v2, ok2 := <-in2
+		for ok1 || ok2 {
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				v1, ok1 = <-in1
+			} else {
+				out <- v2
+				v2, ok2 = <-in2
+			}
+		}
+		close(out)
 	}()
 
 	return out
@@ -54,7 +65,8 @@ func Merge(in1, in2 <-chan int) <-chan int {
 
 func main() {
 	// 以下非阻塞
-	p := InMemorySort(ArraySource(3, 2, 6, 7, 4, 5, 1))
+	p := Merge(InMemorySort(ArraySource(3, 2, 6, 7, 4, 5, 1)), InMemorySort(ArraySource(9, 8, 5, 2, 14, 7, 2)))
+
 	// 阻塞
 	for v := range p {
 		fmt.Println(v)
